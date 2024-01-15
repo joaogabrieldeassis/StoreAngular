917e61e968ed68a7bcff3063e461bac3a3a11344
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2024-01-15 11:22:16 -0300

    Projeto finalizado

--- Store-SPA/src/app/conta/conta.app.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'conta-app-root',
-    templateUrl: './conta.app.component.html'
-})
-export class ContaAppComponent { }
---
+++ front-end/src/app/utils/localstorage.ts
@@ -0,0 +1,30 @@
+export class LocalStorageUtils {
+
+    public obterUsuario() {
+        return JSON.parse(localStorage.getItem('devio.user'));
+    }
+
+    public salvarDadosLocaisUsuario(response: any) {
+
+        this.salvarTokenUsuario(response.accessToken);
+        this.salvarUsuario(response.userTokenViewModel);
+    }
+
+    public limparDadosLocaisUsuario() {
+        localStorage.removeItem('devio.token');
+        localStorage.removeItem('devio.user');
+    }
+
+    public obterTokenUsuario(): string {
+        return localStorage.getItem('devio.token');
+    }
+
+    public salvarTokenUsuario(token: string) {
+        localStorage.setItem('devio.token', token);
+    }
+
+    public salvarUsuario(user: string) {
+        localStorage.setItem('devio.user', JSON.stringify(user));
+    }
+
+}
